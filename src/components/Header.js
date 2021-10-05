b795ded0b560b9b009ac813ca9cf6935eb5125d8
ttony88 <1988anton62gmail.com>
2021-10-05 10:46:20 +0300

    add gitignore

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return( <header className='header'>
+               <img src="https://1000logos.net/wp-content/uploads/2017/05/Emblem-WWF.jpg" alt=""/>
+           </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img src="https://w-dog.ru/wallpapers/9/16/399332228390766/ssha-shtata-vajoming-grand-titon-nacionalnyj-park-gora-moran-ozero-dzhekson-priroda-leto-utro-les-otrazheniya.jpg" alt=""/>
+            </div>
+            <div>ava + description</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    );
+}
+export default Profile;
